0d731358aed98d6ca68861e80fa51eff0b84428e
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-01-30 10:57:56 +0100

    Added config file

--- plc_cpu.py
+++ plc_cpu.py
@@ -1,8 +1,23 @@
+import ipaddress
 import json
+import os
+import random
+import socket
 
 
 def main():
     print('Success')
+
+
+def load_config():
+    directory = os.path.dirname(__file__)
+    filename = os.path.join(directory, 'config.json')
+
+    if not os.path.exists(filename):
+        return {}
+
+    with open(filename, 'r', encoding='utf-8') as fin:
+        return json.load(fin)
 
 
 if __name__ == '__main__':
